58bad523baf43b38ea15da0aa991464e67dae0a0
GuillaumeShrp <guillaume.sherpa@gmail.com>
2020-12-04 16:48:18 +0100

    [GUI] adding frames

--- app/src/main/java/com/plaisir/victor/MainActivity.kt
+++ app/src/main/java/com/plaisir/victor/MainActivity.kt
@@ -20,10 +20,12 @@ class MainActivity : AppCompatActivity() {
     fun changervictor(view: View) {
         if (victorEstNormal) {
             findViewById<ImageView>(R.id.a_main_img_victor).setImageResource(R.drawable.victorheureux)
+            findViewById<ImageView>(R.id.a_main_frame).setImageResource(R.drawable.fire_frame_design)
             findViewById<Button>(R.id.a_main_btn).text = getString(R.string.CalmerVictor)
             Toast.makeText(this, "Hmmm c'est bon", Toast.LENGTH_SHORT).show()
         } else {
             findViewById<ImageView>(R.id.a_main_img_victor).setImageResource(R.drawable.victornormal)
+            findViewById<ImageView>(R.id.a_main_frame).setImageResource(R.drawable.gold_frame_design)
             findViewById<Button>(R.id.a_main_btn).text = getString(R.string.faire_du_bien_a_victor)
             Toast.makeText(this, "Merci mec !", Toast.LENGTH_SHORT).show()
         }
