0e0ea9b6782d630c0fb77514bf942b45c31be62d
GuillaumeShrp <guillaume.sherpa@gmail.com>
2020-12-04 17:29:46 +0100

    [GUI] adding pleasure counter

--- app/src/main/java/com/plaisir/victor/MainActivity.kt
+++ app/src/main/java/com/plaisir/victor/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,10 +12,13 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private var victorEstNormal: Boolean = true
+    private var compteurDePlaisir: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //init counter display:
+        findViewById<TextView>(R.id.a_main_txt_cmpt).text = (getString(R.string.dose_de_plaisir) + "$compteurDePlaisir")
     }
 
     fun changervictor(view: View) {
@@ -23,6 +27,8 @@ class MainActivity : AppCompatActivity() {
             findViewById<ImageView>(R.id.a_main_frame).setImageResource(R.drawable.fire_frame_design)
             findViewById<Button>(R.id.a_main_btn).text = getString(R.string.CalmerVictor)
             Toast.makeText(this, "Hmmm c'est bon", Toast.LENGTH_SHORT).show()
+            compteurDePlaisir ++
+            findViewById<TextView>(R.id.a_main_txt_cmpt).text = (getString(R.string.dose_de_plaisir) + "$compteurDePlaisir")
         } else {
             findViewById<ImageView>(R.id.a_main_img_victor).setImageResource(R.drawable.victornormal)
             findViewById<ImageView>(R.id.a_main_frame).setImageResource(R.drawable.gold_frame_design)
