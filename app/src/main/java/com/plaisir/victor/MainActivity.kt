6cc922d1dd161961743aeae6660dc1d015976bbc
Baptiste <baptiste.massardier@gmail.com>
2020-12-05 16:50:28 +0900

    Plus de plaisir

--- app/src/main/java/com/plaisir/victor/MainActivity.kt
+++ app/src/main/java/com/plaisir/victor/MainActivity.kt
@@ -26,14 +26,14 @@ class MainActivity : AppCompatActivity() {
             findViewById<ImageView>(R.id.a_main_img_victor).setImageResource(R.drawable.victorheureux)
             findViewById<ImageView>(R.id.a_main_frame).setImageResource(R.drawable.fire_frame_design)
             findViewById<Button>(R.id.a_main_btn).text = getString(R.string.CalmerVictor)
-            Toast.makeText(this, "Hmmm c'est bon", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Hmmm c'est bon " + ("\ud83e\udd70"), Toast.LENGTH_SHORT).show()
             compteurDePlaisir ++
             findViewById<TextView>(R.id.a_main_txt_cmpt).text = (getString(R.string.dose_de_plaisir) + "$compteurDePlaisir")
         } else {
             findViewById<ImageView>(R.id.a_main_img_victor).setImageResource(R.drawable.victornormal)
             findViewById<ImageView>(R.id.a_main_frame).setImageResource(R.drawable.gold_frame_design)
             findViewById<Button>(R.id.a_main_btn).text = getString(R.string.faire_du_bien_a_victor)
-            Toast.makeText(this, "Merci mec !", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Merci mec ! " + ("\ud83d\ude44"), Toast.LENGTH_SHORT).show()
         }
         victorEstNormal = !victorEstNormal
     }
