12db64b2a7b1d980bfd74a1971fb602f2dfd8d0d
Baptiste <baptiste.massardier@gmail.com>
2020-12-18 04:20:50 +0900

    la ça marche casse le pas stp

--- app/src/main/java/com/plaisir/victor/IdolAdapter.kt
+++ app/src/main/java/com/plaisir/victor/IdolAdapter.kt
@@ -1,6 +1,5 @@
 package com.plaisir.victor
 
-import android.R
 import android.content.Context
 import android.content.SharedPreferences
 import android.view.LayoutInflater
@@ -18,18 +17,18 @@ class IdolAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IdolViewHolder {
         eContext = parent.context
 
-        val rowView = LayoutInflater.from(parent.context).inflate(R.layout.chi, parent, false)
+        val rowView = LayoutInflater.from(parent.context).inflate(R.layout.element_idol, parent, false)
         return IdolViewHolder(rowView)
     }
 
     override fun onBindViewHolder(holder: IdolViewHolder, position: Int) {
-        val (id, name, img_icon, img_background) = idols[position]
+        val (id, name, img_background, img_icon) = idols[position]
 
         holder.txvIdolName.text = name
         holder.imvIdol.setImageResource(img_icon)
         holder.imvBackground.setImageResource(img_background)
 
-        //((selector_view.getAdapter() as SimpleTabPagerAdapter).getItem(position) as ImageView).setBackgroundResource(R.drawable.p_photo_marked)
+//        ((selector_view.getAdapter() as SimpleTabPagerAdapter).getItem(position) as ImageView).setBackgroundResource(R.drawable.p_photo_marked)
 
 
         //Toast.makeText(eContext, "favList : $isFavoriteVal", Toast.LENGTH_SHORT).show()
